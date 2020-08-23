1d2a031a0740ec79ccd1bf4851717d3a129b943b
pspupsp <pspupsp@hypercol.xyz>
2020-08-23 09:15:38 +0000

    Updated Switcher.glsl

---
+++ shaders/lib/Switcher.glsl
@@ -0,0 +1,24 @@
+#if Shader_Type == frag
+#endif
+
+#if Shader_Type == geom
+#endif
+
+#if Shader_Type == vert
+#endif
+
+
+
+#if World_Type == world0
+#endif
+
+#if World_Type == world1
+#endif
+
+#if World_Type == world_1
+#endif
+
+
+
+#if Shader_Feature == gb_basic
+#endif
