62d775d62d3012de5a81a916c2648d4b26c5e0a5
pspupsp <pspupsp@hypercol.xyz>
2020-08-26 13:05:37 +0000

    Deleted shaders/lib

--- shaders/lib/Syntax.glsl
+++
@@ -1,8 +0,0 @@
-#define Tex(tex, uv) texture(tex, uv)
-#define TexLod(tex, uv, arg) textureLod(tex, uv, arg)
-#define FragData[arg] gl_FragData[arg]
-#define FragColor gl_FragColor
-
-#define pow2(data) (data * data)
-#define pow3(data) (pow2(data) * data)
-#define pow4(data) (pow2(data) * pow2(data))
