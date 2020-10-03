063d7082649fb23819d30efe0c58d48d918e6cc5
pspupsp <pspupsp@hypercol.xyz>
2020-10-03 07:14:22 +0000

    Updated Bridge.glsl

--- shaders/ElegantLib/Bridge.glsl
+++ shaders/ElegantLib/Bridge.glsl
@@ -1,4 +1,16 @@
 #ifndef Bridge_Enabled
 #define Bridge_Enabled
 
+#if Shader_Func == gb_basic
+#endif
+
+#if Shader_Func == gb_textured
+#endif
+
+#if Shader_Func == gb_textured_lit
+#endif
+
+#if Shader_Func == gb_terrain
+#endif
+
 #endif
