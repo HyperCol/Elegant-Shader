88ea810850e894b682a97837968aff637717e797
pspupsp <pspupsp@hypercol.xyz>
2020-10-03 07:12:46 +0000

    Updated Header.glsl

--- shaders/Header.glsl
+++ shaders/Header.glsl
@@ -1,4 +1,57 @@
 #ifndef Header_Enabled
 #define Header_Enabled
 
+//World type
+#define Type_world0  0
+#define Type_world_1 1
+#define Type_world1  2
+
+//Shader Function
+#define gb_basic
+#define gb_textured
+#define gb_textured_lit
+#define gb_terrain
+#define gb_skybasic
+#define gb_skytextured
+#define gb_water
+#define deferred_0
+#define deferred_1
+#define deferred_2
+#define deferred_3
+#define deferred_4
+#define deferred_5
+#define deferred_6
+#define deferred_7
+#define deferred_8
+#define deferred_9
+#define deferred_10
+#define deferred_11
+#define deferred_12
+#define deferred_13
+#define deferred_14
+#define deferred_15
+#define composite_0
+#define composite_1
+#define composite_2
+#define composite_3
+#define composite_4
+#define composite_5
+#define composite_6
+#define composite_7
+#define composite_8
+#define composite_9
+#define composite_10
+#define composite_11
+#define composite_12
+#define composite_13
+#define composite_14
+#define composite_15
+#define ShaderFunc_final
+#define ShaderFunc_shadow
+
+//Shader type
+#define Shader_Frag
+#define Shader_Vert
+#define Shader_Geom
+
 #endif
