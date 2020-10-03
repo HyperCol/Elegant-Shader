2225ebe221d2b1db56bf199770ce5e3d4f9d3c0c
pspupsp <pspupsp@hypercol.xyz>
2020-10-03 07:28:57 +0000

    Updated Header.glsl

--- shaders/Header.glsl
+++ shaders/Header.glsl
@@ -7,51 +7,51 @@
 #define World_world1  2
 
 //Shader Function [Shader_Func]
-#define gb_basic
-#define gb_textured
-#define gb_textured_lit
-#define gb_terrain
-#define gb_skybasic
-#define gb_skytextured
-#define gb_water
-#define deferred_0
-#define deferred_1
-#define deferred_2
-#define deferred_3
-#define deferred_4
-#define deferred_5
-#define deferred_6
-#define deferred_7
-#define deferred_8
-#define deferred_9
-#define deferred_10
-#define deferred_11
-#define deferred_12
-#define deferred_13
-#define deferred_14
-#define deferred_15
-#define composite_0
-#define composite_1
-#define composite_2
-#define composite_3
-#define composite_4
-#define composite_5
-#define composite_6
-#define composite_7
-#define composite_8
-#define composite_9
-#define composite_10
-#define composite_11
-#define composite_12
-#define composite_13
-#define composite_14
-#define composite_15
-#define ShaderFunc_final
-#define ShaderFunc_shadow
+#define gb_basic        100
+#define gb_textured     101
+#define gb_textured_lit 102
+#define gb_terrain      103
+#define gb_skybasic     104
+#define gb_skytextured  105
+#define gb_water        106
+#define deferred_0      200
+#define deferred_1      201
+#define deferred_2      202
+#define deferred_3      203
+#define deferred_4      204
+#define deferred_5      205
+#define deferred_6      206
+#define deferred_7      207
+#define deferred_8      208
+#define deferred_9      209
+#define deferred_10     210
+#define deferred_11     211
+#define deferred_12     212
+#define deferred_13     213
+#define deferred_14     214
+#define deferred_15     215
+#define composite_0     300
+#define composite_1     301
+#define composite_2     302
+#define composite_3     303
+#define composite_4     304
+#define composite_5     305
+#define composite_6     306
+#define composite_7     307
+#define composite_8     308
+#define composite_9     309
+#define composite_10    310
+#define composite_11    311
+#define composite_12    312
+#define composite_13    313
+#define composite_14    314
+#define composite_15    315
+#define ShaderFunc_final  400
+#define ShaderFunc_shadow 401
 
 //Shader type [Shader_Type]
-#define Shader_Frag
-#define Shader_Vert
-#define Shader_Geom
+#define Shader_Frag 0
+#define Shader_Vert 1
+#define Shader_Geom 2
 
 #endif
