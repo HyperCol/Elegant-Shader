60f22e3c176d1754c84e0df0506d8e7e5f7a511b
pspupsp <pspupsp@hypercol.xyz>
2020-10-09 10:17:21 +0000

    Updated Bridge.glsl

--- shaders/ElegantLib/Bridge.glsl
+++ shaders/ElegantLib/Bridge.glsl
@@ -1,4 +1,4 @@
-#ifndef Bridge_Enabled
+#if !defined(Bridge_Enabled) && defined(Header_Enabled)
 #define Bridge_Enabled
 
 #if Shader_Func == gb_basic
@@ -11,6 +11,9 @@
 #endif
 
 #if Shader_Func == gb_terrain
+#endif
+
+#if Shader_Func == gb_water
 #endif
 
 #endif
